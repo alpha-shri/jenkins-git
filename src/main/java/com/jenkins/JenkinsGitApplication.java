813bae5a71ba868796555437f444c3ecd2c7f540
Shrijeet Kumar Panda <shrijeetkumarpanda@gmail.com>
2021-06-21 20:32:52 +0530

    initial commit

---
+++ src/main/java/com/jenkins/JenkinsGitApplication.java
@@ -0,0 +1,28 @@
+package com.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsGitApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsGitApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application started successfully..");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed..");
+
+		SpringApplication.run(JenkinsGitApplication.class, args);
+	}
+
+
+
+}
